7dc49509efde272d821e92135ec15b543f826417
virajjiwane <43500769+virajjiwane@users.noreply.github.com>
2023-05-02 20:26:00 +0530

    added model

--- weatherproject/stats/models.py
+++ weatherproject/stats/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class HistoricalRecords(models.Model):
+    station_code = models.CharField(max_length=11)
+    recorded_on = models.DateField()
+    # Since data is in tenths, having decimal places to 1 and assuming max temperature wont reach 100,
+    # keeping max_digits as 3
+    max_temperature = models.DecimalField(decimal_places=1, max_digits=3)
+    min_temperature = models.DecimalField(decimal_places=1, max_digits=3)
+    precipitation = models.DecimalField(decimal_places=1, max_digits=4)
+
+    class Meta:
+        unique_together = ('station_code', 'recorded_on')
