8840f96a5d3f24047dcee66693fc8609e17db4cd
virajjiwane <43500769+virajjiwane@users.noreply.github.com>
2023-05-04 02:16:49 +0530

    duplicate stats check

--- weatherproject/scripts/generate_statistics.py
+++ weatherproject/scripts/generate_statistics.py
@@ -2,12 +2,47 @@
 This module contains functions to calculate statistics over the DB and populate the statistic DB.
 """
 import operator
+import traceback
 from functools import reduce
+from multiprocessing import cpu_count, Pool
 
-from django.db.models import F, Avg, Q, Sum
+from django.db.models import F, Avg, Q, Sum, Count
 from stats.models import HistoricalRecord, Statistic
 import logging
 logger = logging.getLogger('django')
+
+
+def generate_stats(station_code):
+    """
+    This function generates and inserts stats into DB if not already present.
+    :param station_code:
+    :return: Count of records inserted. Default is 0
+    """
+
+    inserted_record_count = 0
+    try:
+        years = Statistic.objects\
+            .filter(station_code=station_code)\
+            .values('year')\
+            .annotate(Count('year'))\
+            .values_list('year', flat=True)
+        statistics = HistoricalRecord.objects\
+            .filter(station_code=station_code) \
+            .annotate(year=F('recorded_on__year')) \
+            .values('year', 'station_code') \
+            .exclude(year__in=years) \
+            .annotate(avg_max_temperature=Avg('max_temperature', filter=Q(max_temperature__isnull=False)),
+                      avg_min_temperature=Avg('min_temperature', filter=Q(min_temperature__isnull=False)),
+                      total_precipitation=Sum('precipitation'))
+
+        statistic_obj_list = map(lambda s: Statistic(**s), statistics)
+        statistic_obj_list = Statistic.objects.bulk_create(statistic_obj_list)
+        inserted_record_count = len(statistic_obj_list)
+    except:
+        logger.error(f"Error processing {station_code}")
+        traceback.print_exc()
+
+    return inserted_record_count
 
 
 def run():
@@ -16,20 +51,10 @@ def run():
     It also tackles duplicate values or program-reruns.
     """
     logger.info("Generating statistics")
-    existing_stats = Statistic.objects.values('year', 'station_code')
-    if existing_stats:
-        query = reduce(operator.or_, map(lambda es: Q(year=es['year'], station_code=es['station_code']), existing_stats))
-    else:
-        query = Q()
-    statistics = HistoricalRecord.objects\
-        .annotate(year=F('recorded_on__year'))\
-        .values('year', 'station_code')\
-        .exclude(query)\
-        .annotate(avg_max_temperature=Avg('max_temperature', filter=Q(max_temperature__isnull=False)),
-                  avg_min_temperature=Avg('min_temperature', filter=Q(min_temperature__isnull=False)),
-                  total_precipitation=Sum('precipitation'))
+    stations = HistoricalRecord.objects.values('station_code').annotate(Count('station_code')).values_list('station_code', flat=True)
+    logger.info(f'Using {cpu_count()} processes in parallel')
+    pool = Pool(cpu_count())
+    inserted_records_count = sum(pool.map(generate_stats, stations))
 
-    statistic_obj_list = map(lambda s: Statistic(**s), statistics)
-    statistic_obj_list = Statistic.objects.bulk_create(statistic_obj_list)
-    logger.info(f"{len(statistic_obj_list)} stats inserted")
+    logger.info(f"{inserted_records_count} stats inserted")
     logger.info("Generated statistics")
--- weatherproject/stats/admin.py
+++ weatherproject/stats/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import HistoricalRecord
+from .models import HistoricalRecord, Statistic
 
 admin.site.register(HistoricalRecord)
+admin.site.register(Statistic)
